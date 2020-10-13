fbb6546e624b6af4f3ecb703e786c5d7a1e89e23
Florent <flgallois@gmail.com>
2020-10-13 13:04:15 +0200

    CORRECTION: SIMPLIFY PARAMETERS

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,29 +1,25 @@
 <template>
   <div class="calculator">
     <div class="display"> {{ current }}</div>
-    <div @click="parenthesis('(')" class="btn">(</div>
-    <div @click="parenthesis(')')" class="btn">)</div>
-    <div class="btn"></div>
-    <div class="btn"></div>
     <div @click="clear" class="btn">C</div>
-    <div @click="sign" class="btn">+/-</div>
-    <div @click="pourcent" class="btn">%</div>
+    <div @click="openParenth('(')" class="btn">(</div>
+    <div @click="closeParenth(')')" class="btn">)</div>
     <div @click="operand('/')" class="operator">/</div>
-    <div @click="append('7')" class="btn">7</div>
-    <div @click="append('8')" class="btn">8</div>
-    <div @click="append('9')" class="btn">9</div>
-    <div @click="operand('x')" class="operator">x</div>
-    <div @click="append('4')" class="btn">4</div>
-    <div @click="append('5')" class="btn">5</div>
-    <div @click="append('6')" class="btn">6</div>
+    <div @click="appendNumber('7')" class="btn">7</div>
+    <div @click="appendNumber('8')" class="btn">8</div>
+    <div @click="appendNumber('9')" class="btn">9</div>
+    <div @click="operand('*')" class="operator">x</div>
+    <div @click="appendNumber('4')" class="btn">4</div>
+    <div @click="appendNumber('5')" class="btn">5</div>
+    <div @click="appendNumber('6')" class="btn">6</div>
     <div @click="operand('-')"  class="operator">-</div>
-    <div @click="append('1')" class="btn">1</div>
-    <div @click="append('2')" class="btn">2</div>
-    <div @click="append('3')" class="btn">3</div>
+    <div @click="appendNumber('1')" class="btn">1</div>
+    <div @click="appendNumber('2')" class="btn">2</div>
+    <div @click="appendNumber('3')" class="btn">3</div>
     <div @click="operand('+')" class="operator">+</div>
-    <div @click="append('0')" class="zero btn">0</div>
+    <div @click="appendNumber('0')" class="zero btn">0</div>
     <div @click="dot()" class="btn">.</div>
-    <div @click="comput('=')" class="operator">=</div>
+    <div @click="displayResult()" class="operator">=</div>
   </div>
 </template>
 
@@ -31,120 +27,137 @@
 export default {
   data: function () {
     return {
-      operator: '',
-      current: '0',
-      isLastCharNumber: true,
-      isOperandAllowed: false,
+      lastchar: '0',
+      operations: ['+', '-', '*', '/'],
+      parenthesis: ['(', ')'],
       openParenthesis: 0,
-      isDotAllowed: true
+      countDotinCurrentNb: 0,
+      current: '0'
+      // current: '0',
+      // isParenthesisAllowed: true,
+      // isOperandAllowed: false,
+      // isDotAllowed: true,
+      // isNumberAllowed: true,
     }
   },
   methods: {
     dot: function () {
-      console.log(this.isDotAllowed)
-      console.log(this.isLastCharNumber)
-      if (this.current === '0') this.current = '0.'
-      else if (this.isLastCharNumber && this.isDotAllowed) this.current = `${this.current}.`
-      this.isDotAllowed = false
+      if (this.dotAllowed) {
+        this.lastchar = '.'
+        this.current = `${this.current}.`
+        this.countDotinCurrentNb++
+      }
     },
     clear: function () {
       this.current = '0'
-      this.previous = '0'
-      this.isDotAllowed = true
+      this.lastchar = '0'
+      this.openParenthesis = 0
+      this.countDotinCurrentNb = 0
     },
-    parenthesis: function (value) {
-      if (this.current === '0' && value === '(') {
-        this.current = `${value}`
-        this.isDotAllowed = true
-        this.isOperandAllowed = false
-        this.isLastCharNumber = false
-        this.openParenthesis++
-      } else if (!this.isLastCharNumber && value === '(') {
-        this.current = `${this.current}${value}`
-        this.isOperandAllowed = false
-        this.isLastCharNumber = false
-        this.isDotAllowed = true
-        this.openParenthesis++
-      } else if (this.isLastCharNumber && value === ')' && this.openParenthesis > 0) {
-        this.current = `${this.current}${value}`
-        this.isOperandAllowed = true
-        this.isLastCharNumber = false
-        this.isDotAllowed = true
+    openParenth: function () {
+      console.log(this.openParenthesisAllowed)
+      if (this.openParenthesisAllowed) {
+         this.lastchar = '('
+         this.countDotinCurrentNb = 0
+         this.openParenthesis++
+        if (this.current === '0') this.current = '('
+        else this.current = this.current + '('
+       
+      }
+    },
+    closeParenth: function () {
+        console.log(this.closeParenthesisAllowed)
+      if (this.closeParenthesisAllowed) {
         this.openParenthesis--
+        this.lastchar = ')'
+        this.current = this.current + ')'
+        this.countDotinCurrentNb = 0
       }
     },
     operand: function (value) {
-      if (this.current === '0' && value === '-') {
-        this.current = `${value}`
-        this.isLastCharNumber = false
-        this.isOperandAllowed = false
-        this.isDotAllowed = true
-      }
-      if (this.isOperandAllowed) {
-        this.current = `${this.current}${value}`
-        this.isLastCharNumber = false
-        this.isOperandAllowed = false
-        this.isDotAllowed = true
+      console.log(this.operandAllowed)
+      if (this.operandAllowed || (this.current === '0' && value === '-')) {
+        if (this.current === '0') this.current = '-'
+        else {
+          this.current = this.current + value
+        }
+         this.countDotinCurrentNb = 0
+         this.lastchar = value
       }
     },
-    append: function (value) {
-      this.isLastCharNumber = true
-      this.isOperandAllowed = true
-      if (this.current !== '0') this.current = `${this.current}${value}`
-      else {
-        this.current = `${value}`
+    appendNumber: function (value) {
+      console.log(this.numberAllowed)
+      if (this.numberAllowed) {
+        this.lastchar = value
+        if (this.current !== '0') this.current = `${this.current}${value}`
+        else {
+          this.current = `${value}`
+        }
+        this.lastchar = value
       }
     },
-    sign: function () {
-      if (this.current !== '0') {
-        this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${this.current}`
-      }
-    },
-    pourcent: function (value) {
-      this.current = `${parseFloat(this.current) / 100}`
-    },
-    comput: function (c) {
-      if (c === '=') {
-        let res = this.calculateResult(this.current)
-        this.current = res
-        console.log('---->' + res)
-      }
-    },
-    calculateResult: function (word) {
+    displayResult: function () {
+        this.current = this.comput(this.current)
+      },
+    comput: function (word) {
       console.log(word)
-      let operations = [')', '+', '-', '/', 'x']
+      let characters = [')', '+', '-', '/', '*']
       let i = 0
       if (!word) return 0
-      while (operations[i]) {
+      while (characters[i]) {
         console.log('I - ' + i)
-        // console.log(operations[i])
-        let ind = word.indexOf(operations[i])
-        console.log('OCCURENCE')
-        console.log(operations[i])
-        console.log(ind)
+        // console.log(characters[i])
+        let ind = word.indexOf(characters[i])
         if (ind !== -1) {
           if (i === 0) {
             let lastOpenParenthesis = word.lastIndexOf('(', word.substr(0, ind))
             if (lastOpenParenthesis !== -1) {
-              return this.calculateResult(word.substr(0, lastOpenParenthesis) + toString(this.calculateResult(word.substr(lastOpenParenthesis + 1, ind - lastOpenParenthesis - 1))) + word.substr(ind + 1, word.length))
+              console.log('(')
+              return this.comput(word.substr(0, lastOpenParenthesis) + this.comput(word.substr(lastOpenParenthesis + 1, ind - lastOpenParenthesis - 1)) + word.substr(ind + 1, word.length))
             } else return 0
           }
           if (i === 1) {
-            return this.calculateResult(word.substr(0, ind)) + this.calculateResult(word.substr(ind + 1, word.length))
+            console.log('+')
+            return this.comput(word.substr(0, ind)) + this.comput(word.substr(ind + 1, word.length))
           }
           else if (i === 2) {
-            return this.calculateResult(word.substr(0, ind)) - this.calculateResult(word.substr(ind + 1, word.length))
+             console.log('-')
+            return this.comput(word.substr(0, ind)) - this.comput(word.substr(ind + 1, word.length))
           }
           else if (i === 3) {
-            return this.calculateResult(word.substr(0, ind)) / this.calculateResult(word.substr(ind + 1, word.length))
+                console.log('/')
+            return this.comput(word.substr(0, ind)) / this.comput(word.substr(ind + 1, word.length))
           }
           else if (i === 4) {
-            return this.calculateResult(word.substr(0, ind)) * this.calculateResult(word.substr(ind + 1, word.length))
+                console.log('x')
+            return this.comput(word.substr(0, ind)) * this.comput(word.substr(ind + 1, word.length))
           }
         }
         i++
       }
       return parseFloat(word)
+    }
+  },
+  computed: {
+    operandAllowed: function () {
+      if ( this.current !== '0' && !this.operations.includes(this.lastchar) && this.lastchar !== '(') return true
+      return false
+
+    },
+    numberAllowed: function () {
+      return this.lastchar !== ')'
+    },
+    openParenthesisAllowed: function () {
+      if (this.operations.includes(this.lastchar) || this.current === '0') return true
+      return false
+    },
+    closeParenthesisAllowed: function () {
+      if (this.openParenthesis > 0 && !this.operations.includes(this.lastchar)) return true
+      return false
+    },
+    dotAllowed: function () {
+      if (!this.operations.includes(this.lastchar) && !this.parenthesis.includes(this.lastchar) && this.countDotinCurrentNb === 0) return true
+      return false
     }
   }
 }
